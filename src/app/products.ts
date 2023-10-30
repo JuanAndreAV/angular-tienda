02391d4dd0a9ef711e3fb3987970516f1d07cf09
Juan Andres Alvarez <juanandres1029@gmail.com>
2023-10-29 21:37:18 -0500

    app

---
+++ src/app/products.ts
@@ -0,0 +1,34 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Viola',
+    price: 500.000,
+    description: 'Sonido calido, mejor que el del violín'
+  },
+  {
+    id: 2,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    id: 3,
+    name: 'Phone Standard',
+    price: 299,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
